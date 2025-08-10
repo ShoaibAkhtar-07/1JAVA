7ba003830de11b9a9b14536828da69760d5d2d89
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-08-10 11:26:24 +0530

    Create Pattern.java

---
+++ Pattern.java
@@ -0,0 +1,44 @@
+// import java.util.*;
+// public class Pattern {
+//     public static void main(String[] args){
+//         Scanner sc = new Scanner(System.in);
+//         System.out.print("Enter the no.of ROWS : ");
+//         int r = sc.nextInt();
+//         System.out.print("Enter the no.of COLUMNS : ");
+//         int c = sc.nextInt();
+
+//         for(int i = 1;i<= r;i++){
+//             for(int j =1;j<=c;j++){
+//                 System.out.print("*");
+//             }
+//             System.out.println();
+//         }
+//     }
+// }
+
+
+import java.util.*;
+public class Pattern {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the no.of rows : ");
+        int r = sc.nextInt();
+        System.out.print("Enter the no.of COLUMNS : ");
+        int c = sc.nextInt();
+
+        for(int i = 1;i<=r;i++){
+            if(i == 1 || i == r){
+                for(int j = 1; j <= c;j++){
+                    System.out.print("*");
+                }
+            }else{
+                System.out.print("*");
+                for(int j=1;j<=c-2;j++){
+                    System.out.print(" ");
+                }
+                System.out.print("*");
+            }
+            System.out.println();
+        }
+    }
+}
