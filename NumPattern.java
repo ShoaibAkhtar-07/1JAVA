2eab2521652e954ea0eb3e148c5bda41713193e0
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-08-11 00:21:15 +0530

    Create NumPattern.java

---
+++ NumPattern.java
@@ -0,0 +1,31 @@
+// import java.util.*;
+// public class NumPattern {
+// public static void main(String[] args){
+//     Scanner sc = new Scanner(System.in);
+//     System.out.print("Enter the number : ");
+//     int n = sc.nextInt();
+//     for(int i =1;i <= n;i++){
+//         for(int j = 1;j<=i;j++){
+//             System.out.print(j);
+//         }
+//         System.out.println();
+//     }
+// }
+    
+// }
+
+
+import java.util.*;
+public class NumPattern {
+    public static void main(String[] args){
+        Scanner sc = new Scanner(System.in);
+        System.out.print("Enter the Number for pattern : ");
+        int n = sc.nextInt();
+        for(int i = n; i>=1 ; i--){
+            for(int j = 1; j<=i ; j++){
+                System.out.print(j+" ");
+            }
+            System.out.println();
+        }
+    }
+}
