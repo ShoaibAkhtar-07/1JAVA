b9d647c2e07f809b264e5be99b9b1c5c81e32619
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-09-07 23:13:24 +0530

    Create Interface.java

---
+++ Interface.java
@@ -0,0 +1,42 @@
+interface AnimalSound {
+    int a = 7;
+    public void sound();
+}
+
+interface AnimalFood  {
+    public void food();
+}
+
+class Dog implements AnimalSound,AnimalFood{
+    public void sound(){
+        System.out.println("Bow Bow");
+    }
+    public void food(){
+        System.out.println("Flesh ");
+
+    }
+}
+
+class Cat implements AnimalSound,AnimalFood{
+    public void sound(){
+        System.out.println("Meow Meow");
+    }
+    public void food(){
+        System.out.println("Milk");
+    }
+}
+
+
+public class Interface {
+    public static void main(String[] args){
+        Cat anc = new Cat();
+        anc.sound();
+        anc.food();
+        System.out.println(anc.a);
+
+        Dog and = new Dog();
+        and.sound();
+        and.food();
+        System.out.println(and.a);
+    }
+}
