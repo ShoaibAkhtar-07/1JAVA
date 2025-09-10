f3825eb094587336b69bc6417b4461fab94af6c3
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-09-10 21:37:45 +0530

    Create InterpretedVsCompiled.java
    About interpreted and complied language ... and why Java is a  hybrid language

---
+++ InterpretedVsCompiled.java
@@ -0,0 +1,21 @@
+/*Interpreter Vs Compiler
+  Interpreter translates one statement at a time into machine code.
+  Compiler Scans the entire program and translates whole of it into machine code.
+
+  INTERPRETER
+  1. One statement at a time. 
+  2. Interpreter is needed everytime.
+  3. Partial execution if error.
+  4. Usually easy for programmers.
+
+  COMPILER
+  1. Entire program at a time.
+  2. Once complied it is not needed.
+  3. No execution if an error occurs.
+  4. Usually not as easy as Interpreter.
+
+
+  IS JAVA COMPILED OR INTERPRETER ?
+  Java is a hybrid language it is both Compiled as well as interpreted.
+
+*/
