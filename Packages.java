4620bf5f42a85d964d123c48147f0e3e0bebdb40
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-09-13 12:13:28 +0530

    Create Packages.java

---
+++ Packages.java
@@ -0,0 +1,13 @@
+package shoaib;
+// import java.util.Scanner;
+import java.util.*; // import everything from java language
+
+public class Packages {
+    public static void main(String[] args) {
+        // java.util.Scanner SC =  new java.util.Scanner(System.in);
+        Scanner sc = new Scanner(System.in);
+        int a = sc.nextInt();
+        System.out.println("The scanner value takes the value " +a  );
+    }
+    
+}
