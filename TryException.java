85e8ee75de73f7a79f94e93f83707c592aaa0abc
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-10-13 19:57:27 +0530

    Create TryException.java

---
+++ TryException.java
@@ -0,0 +1,24 @@
+import java.util.*;
+public class TryException {
+    public static void main(String[] args) {
+        int [] marks = new int[3];
+        marks[0] = 1;
+        marks[1] = 4;
+        marks[2] = 3;
+        Scanner sc = new Scanner(System.in);
+        System.out.println("Enter the index of array : ");
+        int ind = sc.nextInt();
+
+        System.out.println("Enter the Number by which you want to divide : ");
+        int num = sc.nextInt();
+        try{
+            System.out.println("The value at array index which you have entered is : "+marks[ind]);
+            System.out.println("division of array  value by given number : "+marks[ind]/num);
+
+        }
+        catch(Exception e){
+            System.out.println("Some error occured");
+            System.out.println(e);
+        }
+    }
+}
