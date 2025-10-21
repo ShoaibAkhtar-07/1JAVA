e6266a8baa395243744d500035a708bf3f518e72
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-10-22 01:00:46 +0530

    Create FileHandling.java

---
+++ FileHandling.java
@@ -0,0 +1,22 @@
+ import java.io.File;
+import java.io.FileNotFoundException;
+import java.io.IOException;
+import java.util.Scanner;
+public class FileHandling {
+    public static void main(String[] args) {
+        
+        try {
+        File myObj = new File("students.txt");
+            if (myObj.createNewFile()) {
+                System.out.println("Created Successfully: " + myObj.getName());
+            } else {
+                System.out.println("Sorry, File Exists.");
+            }
+        } catch (IOException e) {
+            System.out.println("Error.....");
+            e.printStackTrace();
+        } 
+
+    }
+}
+
