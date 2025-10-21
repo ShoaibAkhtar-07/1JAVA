daca28c639c6988a3419fd00edce471610a8f1e8
ShoaibAkhtar-07 <shoaibakhtar987987@gmail.com>
2025-10-21 23:00:13 +0530

    Update TryException.java

--- TryException.java
+++ TryException.java
@@ -12,19 +12,21 @@ public class TryException {
         System.out.println("Enter the Number by which you want to divide : ");
         int num = sc.nextInt();
         try{
-            System.out.println("The value at array index which you have entered is : "+marks[ind]);
-            System.out.println("division of array  value by given number : "+marks[ind]/num);
-
-        }
-        catch (ArithmeticException e){
-            System.out.println(e);
-        }
-        catch (ArrayIndexOutOfBoundsException e){
-            System.out.println(e);
+            System.out.println("I am in level 1 try and  exception");
+            try{
+                System.out.println("The value at array index which you have entered is : "+marks[ind]);
+                System.out.println("division of array  value by given number : "+marks[ind]/num);
+            }
+            catch (ArithmeticException e){
+                System.out.println(e);
+            }
+            catch (ArrayIndexOutOfBoundsException e){
+                System.out.println(e);
+            }
         }
         catch(Exception e){
-            System.out.println("Some error occured");
             System.out.println(e);
         }
     }
+
 }
